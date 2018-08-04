7f9c8957f4478aa5c2eee56b9cec8d6ffb56ebf7
Tim McGowan <30318479+timmcgowan@users.noreply.github.com>
2018-08-04 19:36:50 -0400

    more player action

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,18 @@
 //Define Global Vars
 var endgame = false; // control end/restart
+var champ; //selected
 var champ_health = 0;
 var champ_attack = 0;
 var champ_img;
-var champ_sel = false; 
+var champ_sel = false;
+var opp; 
 var opp_health = 0;
 var opp_attack = 0;
+var opp_img;
 var charType = "notselected"
 var wins;
 var looses;
+var _battle = false;
 var opp_sel = false; //opponent selected false
 var debug = true; // for console debug output
 var log_char = false; // console log characters
@@ -47,6 +51,18 @@ try {
     charObj = character;
 }
 
+function playerArea(){
+    let l = charObj.length;
+    for (let index = 0; index < l; index++) {
+        let _span = $("<span class='character-img' id='C"+ index +"'>");
+        let _name = $("<p class='character-name'>"+ charObj[index].name + "</p>");
+        let _img = $("<img onclick=\"selectPlayer(\'" + index + "\')\" src='"+ charObj[index].img + "' alt='" + charObj[index].name + "' class='character-img'>");
+        let _health = $("<p class='health'>" + charObj[index].health + "</p>");
+        _span.append(_name).append(_img).append(_health);
+        $("#characters").append(_span);   
+    }        
+}
+
 // verifying character outputs
 if(log_char){
     var i;
@@ -58,14 +74,18 @@ if(log_char){
         console.log("img: " + charObj[i].img);
     }
 }
-function selectPlayer (id){
+
+function selectPlayer(id){
     if (!champ_sel){
         champ_sel = true;
         renderHtml.player(id,"champion");
+        renderHtml.vs();
+        champ = id;
     } else if (!opp_sel){
         opp_sel = true;
         renderHtml.player(id,"opponent");
         renderHtml.battleButton();
+        opp = id;
     } else {
         if(debug){console.log("Both Players already selected")}
         return
@@ -73,90 +93,90 @@ function selectPlayer (id){
     return
 
 }
+
 var renderHtml = {
-        //Choose a Champion
         player: function(cid,charType){
             var hidespan = "C" + cid;
-            console.log(hidespan);
             $("#" + hidespan + "").hide();
-            let _div = $("<div class='character' data-name='" + charObj[cid].name + "'>");
+            let _div = $("<div class='character "+ charType +"' id='" + cid + "'>");
             let _name = $("<span class='character-name'>").text(charObj[cid].name);
-            let _img = $("<img class='character-img'>").attr("src", charObj[cid].img);
-            let _health = $("<p class='character-health'>").text(charObj[cid].health);
-            if (charType==="champion"){
-                $(_div).attr('id','champion');
-            } else if (charType==="opponent"){
-                $(_div).attr('id','opponent');
-            } else {
-                $(_div).addClass('contestant');
-            }
+            let _img = $("<img class='" + charType +"-img'>").attr("src", charObj[cid].img);
+            let _health = $("<p class='health'>").text(charObj[cid].health);
             _div.append(_name).append(_img).append(_health);
             $("#gamearea").append(_div);
+        },
+        //Choose a Champion
+        challengers: function(){
+            let l = charObj.length;
+            for (let index = 0; index < l; index++) {
+                renderHtml.player(index, "contestant", "challenge_area");
+            }            
         },
         battleButton: function (){
             let _bbutton = $('<button/>', {
                 text: "Attack",
                 id: 'attack_button',
-                click: function () { battle(); }
+                click: function () { battle(champ,opp); }
             });
             //_bbutton.addClass('atttack_button')
-            $("#gamearea").append(_bbutton);
+            $("#vs").append(_bbutton);
+        },
+        vs: function(){
+            let _vdiv = $("<div id='vs'>");
+            let _vsp = $("<span class='emoji'>").text("🆚");
+            _vdiv.append(_vsp);
+            $("#gamearea").append(_vdiv);
         }
-}
-
-// stats *may no longer need..  
-function champion(cid){
-    if(debug){console.log("cid: " + cid)}
-    if(!champ_sel){
-       champ_health = charObj[cid].health;
-       champ_attack = charObj[cid].attack; //base attack power
-       champ_img = charObj[cid].img;
-       champ_sel = true;
-    }
-    if(debug){console.log("Champion health: " + champ_health + " Attack Power: " + champ_attack)}
-}
-
-//Choose an Opponent 
-function opponent(oid){
-    if(debug){console.log("oid: " + oid)}
-    if (!opp_sel){
-    opp_health = charObj[oid].health;
-    opp_attack = charObj[oid].attack;
-    //opp_img = charObj[oid].img;
-    opp_sel = true;
-    }
-    if(debug){console.log("Opponent Health: " + opp_health + " Opponent Attack :" + opp_attack)}
 }
 
 //Attack (calculations)
 //Opponent Health & Attack
 function battle(_champ,_opp){
-    //update competitor stats
    // renderHtml.player(_champ,"champion")
-    champion(_champ);
-    opponent(_opp);
-//renderHtml.player(_opp,"opponent")
-
+    //update competitor stats
+    if  (!champ_sel && !opp_sel){
+        return false;
+    } else {
+        if (!_battle){
+            if(debug){console.log("cid: " + _champ)}
+            champ_health = charObj[_champ].health;
+            champ_attack = charObj[_champ].attack; //base attack power
+            if(debug){console.log("Champion health: " + champ_health + " Attack Power: " + champ_attack)}
+            //Choose an Opponent 
+            if(debug){console.log("oid: " + _opp)}
+            opp_health = charObj[_opp].health;
+            opp_attack = charObj[_opp].attack;
+            opp_sel = true;
+            if(debug){console.log("Opponent Health: " + opp_health + " Opponent Attack :" + opp_attack)}
+            _battle = true;
+        }
+    console.log("champattack : " + champ_attack );
     //Battle Calculations
     // Champion's Move
     opp_health = opp_health - champ_attack; //Champion Base Attack on Oppent Health
+    console.log("_opp health : " + opp_health );
+
     champ_attack = champ_attack + charObj[_champ].attack; // Increase base attack
+    console.log("_champ attack : " + champ_attack );
+
     // Opponent Move
     if (opp_health <= 0){
         //opponent loses
         console.log("You win!, Your oppenent lost.");
+        $("#" + _opp).remove();
+        $("#attack_button").remove();
         wins++;
     } else {
         champ_health = champ_health - opp_attack;    //decrease health by opponent attack
     }
-    if(debug){console.log("New Health: " + champ_health + " New Attack: " + champ_attack)}
+    if(debug){console.log("New Health: " + champ_health + " New Attack: " + champ_attack)}  
 }
 
+}
 //Score Card?
 
 //Determine Win/Loss
 // if health less than 0, LOSE
 // if all oppen 
-
 
 //Restart Game
